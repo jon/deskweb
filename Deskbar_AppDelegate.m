9114974b71058f0866f9074c3e5696a359de07c6
Jon Olson <jon@death.local>
2008-05-28 16:24:23 -0400

    Added ability to specify size/position in Info.plist

--- Deskbar_AppDelegate.m
+++ Deskbar_AppDelegate.m
@@ -21,7 +21,26 @@ extern char **environ;
 		NSLog(@"DWHomePage not set in Info.plist. Please set this to a string and try again");
 		exit(1);
 	}
-		
+	
+	NSRect screenFrame = [[NSScreen mainScreen] frame];
+	NSRect appFrame = [mainWindow frame];
+	
+	NSString *sizeString = [infoDictionary objectForKey:@"DWSize"];
+	NSArray *sizeComponents = [sizeString componentsSeparatedByString:@"x"];
+	if ([sizeComponents count] == 2) {
+		appFrame.size.width = [[sizeComponents objectAtIndex:0] intValue];
+		appFrame.size.height = [[sizeComponents objectAtIndex:1] intValue];
+	}
+
+	NSString *positionString = [infoDictionary objectForKey:@"DWPosition"];
+	NSArray *positionComponents = [positionString componentsSeparatedByString:@","];
+	if ([positionComponents count] == 2) {
+		appFrame.origin.x = [[positionComponents objectAtIndex:0] intValue];
+		appFrame.origin.y = screenFrame.size.height - [[positionComponents objectAtIndex:1] intValue] - appFrame.size.height;
+	}
+	
+	[mainWindow setFrame:appFrame display:YES];
+	
 	[[webView mainFrame] loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:deskWebHome]]];
 }
 
